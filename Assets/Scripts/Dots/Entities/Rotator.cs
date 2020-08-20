6f0b86f7af07ddb0e3c945d3953b695894d3a13d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-08-20 11:52:05 +0900

    回転用Entityを定義。

---
+++ Assets/Scripts/Dots/Entities/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+
+namespace Dots.Entities
+{
+    /// <summary>
+    /// 回転Entity
+    /// </summary>
+    public class Rotator : IComponentData
+    {
+        public float Speed;
+    }
+}
