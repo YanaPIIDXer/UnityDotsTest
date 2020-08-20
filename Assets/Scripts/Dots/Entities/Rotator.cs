b23074a1bc9b9bbea770368a8d6d564455e2bb89
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-08-20 11:52:57 +0900

    属性が抜けていたので追記。

--- Assets/Scripts/Dots/Entities/Rotator.cs
+++ Assets/Scripts/Dots/Entities/Rotator.cs
@@ -5,6 +5,7 @@ namespace Dots.Entities
     /// <summary>
     /// 回転Entity
     /// </summary>
+    [GenerateAuthoringComponent]
     public class Rotator : IComponentData
     {
         public float Speed;
