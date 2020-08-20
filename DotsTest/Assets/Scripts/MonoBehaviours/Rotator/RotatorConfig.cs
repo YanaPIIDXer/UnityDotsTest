e77c401f86673219cd85d8a95b023d9c23dab952
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-08-20 11:32:49 +0900

    単純に回転させるだけのComponentをMonoBehaviourだけで実装。

---
+++ DotsTest/Assets/Scripts/MonoBehaviours/Rotator/RotatorConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 単純な回転処理の設定値
+/// </summary>
+public static class RotatorConfig 
+{
+    /// <summary>
+    /// 回転速度（毎秒）
+    /// </summary>
+    public static float RotateSpeed = 90.0f;
+}
---
+++ DotsTest/Assets/Scripts/MonoBehaviours/Rotator/SimpleRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// MonoBehaviourだけで回転を実装
+/// </summary>
+public class SimpleRotator : MonoBehaviour
+{
+    /// <summary>
+    /// １秒間の回転量
+    /// </summary>
+    private static readonly Vector3 RotateValueBySec = new Vector3(0.0f, -RotatorConfig.RotateSpeed, 0.0f);
+    
+    protected void Update()
+    {
+        transform.localEulerAngles += RotateValueBySec * Time.deltaTime;
+    }
+}
